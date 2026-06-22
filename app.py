b339441fccbf6acec1cb6986a33e050c8590fea1
tanya499 <tanya130205@gmail.com>
2026-06-23 00:30:33 +0530

    updated model

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import google.generativeai as genai
 
 genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
 
-model = genai.GenerativeModel("models/gemini-1.5-flash")
+model = genai.GenerativeModel("gemini-pro")
 
 app = Flask(__name__)
 
