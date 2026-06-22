b23e6420d21e4ecb5262207583aba2edfb4921ea
tanya499 <tanya130205@gmail.com>
2026-06-23 00:12:31 +0530

    Fixed Gemini model

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import google.generativeai as genai
 
 genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
 
-model = genai.GenerativeModel("gemini-1.5-flash")
+model = genai.GenerativeModel("gemini-1.5-flash-latest")
 
 app = Flask(__name__)
 
